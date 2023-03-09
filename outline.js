ee85de6f310109db319955581b18969ef8da838e
Vxcaroda <davidvicaro@gmail.com>
2023-03-09 12:00:11 -0500

    laptop commits

--- outline.js
+++ outline.js
@@ -1,23 +1,24 @@
 const container1 = document.querySelector(".container1");
 const container2 = document.querySelector(".container2");
 const searchBtn = document.querySelector(".search-box button"); // select the search button element
-const nfimg = document.querySelector(".nfimg");
+const WeatherNovaImg = document.querySelector(".WeatherNovaImg");
 const cityNF = document.querySelector(".city-not-found-blurb");
 let errorFlag = false;
-// const locationP = document.querySelector(".city-not-found-blurb");
+var cityPhoto = new Image();
 
 searchBtn.addEventListener("click", () => {
   // add a click event listener to the search button
   const APIKey = "5c3412626cefebb5a1e1f2156cb1c7ba";
   const city = document.querySelector(".search-box input");
   console.log("Clicked!");
-  if (city.value === "") {
+  city_name = city.value;
+  if (city_name === "") {
     // check if the input value is empty
     return;
   }
 
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=${APIKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=${APIKey}`
   )
     .then((response) => response.json())
     .then((json) => {
@@ -33,21 +34,40 @@ searchBtn.addEventListener("click", () => {
       const image = document.querySelector(".weather-box img");
       const temperature = document.querySelector(
         ".weather-box .temperature-value"
-      ); 
+      );
       //set a variable temperature to the span (horizontal structure) named: temperature-value
       const description = document.querySelector(".description-value");
       const humidity = document.querySelector(".humidity-value");
       const windspeed = document.querySelector(".windspeed-value");
       // const errorwindow = document.querySelector(".city-not-found-blurb");
-      // nfimg.classList.add("hidden");
+      // WeatherNovaImg.classList.add("hidden");
 
-      console.log(city.value);
+      console.log(city_name);
+      console.log(
+        `https://www.google.com/search?client=opera-gx&q=${city_name}&sourceid=opera&ie=UTF-8&oe=UTF-8`
+      );
+      // cityPhoto.src = `https://www.google.com/search?client=opera-gx&q=${city_name}&sourceid=opera&ie=UTF-8&oe=UTF-8`;
+      // cityPhoto.onload = function() {
+      //   document.getElementById("city-photo").src = cityPhoto.src;
+      // };
+      // Convert the city name to lowercase and remove any spaces
+      // var citySlug = city_name.toLowerCase().replace(/\s/g, "-");
+
+      // // Create the URL for the image using the city slug
+      // var imageUrl = "https://www.google.com/search?client=opera-gx&q=" +citySlug+ "&sourceid=opera&ie=UTF-8&oe=UTF-8/";
+      
+
+      // // Create a new Image object and set its source to the URL of the image
+      // var cityPhoto = new Image();
+      // cityPhoto.src = imageUrl;
+      // console.log(imageUrl);
+
       console.log(json);
 
-      if(json.message == "city not found"){
+      if (json.message == "city not found") {
         cityNF.innerHTML = `Sorry! City not found. Try again.`;
         errorFlag = true;
-      } 
+      }
 
       // switch (json.weather[0].main) {
       //   case "Clear":
